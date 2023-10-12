9cc2078ca53a2fe59fa2dcdc8c70f601af446e10
AnilEnsari <anilensari@gmail.com>
2023-10-12 12:26:21 +0300

    all done

--- src/com/workintech/libraryChallenge/Person/FinancialCheck.java
+++ src/com/workintech/libraryChallenge/Person/FinancialCheck.java
@@ -7,19 +7,19 @@ import java.util.List;
 
 public class FinancialCheck {
 
-   private Double currentBalance= 0.00;
+    private Double currentBalance = 0.00;
     private List<Double> payments;
-    private List <Double> debts ;
-    private Double totalPayment= 0.00;
-    private  Double totalDebt = 0.00 ;
-    private List<Book> takenBooks ;
-    private List <Book> givenBooks ;
-    public FinancialCheck(){
+    private List<Double> debts;
+    private Double totalPayment = 0.00;
+    private Double totalDebt = 0.00;
+    private List<Book> takenBooks;
+    private List<Book> givenBooks;
+
+    public FinancialCheck() {
         payments = new ArrayList<>();
         debts = new ArrayList<>();
         takenBooks = new ArrayList<>();
         givenBooks = new ArrayList<>();
-
 
 
     }
@@ -27,10 +27,8 @@ public class FinancialCheck {
     public Double getCurrentBalance() {
 
 
-
         return currentBalance;
     }
-
 
 
     public List<Double> getPayments() {
@@ -40,16 +38,15 @@ public class FinancialCheck {
     }
 
 
-
     public List<Double> getDebts() {
         return debts;
     }
 
     public Double getTotalPayment() {
         totalPayment = 0.0;
-        if( payments != null ){
+        if (payments != null) {
 
-            for (Double payment : payments){
+            for (Double payment : payments) {
 
                 totalPayment += payment;
             }
@@ -61,46 +58,70 @@ public class FinancialCheck {
 
     public Double getTotalDebt() {
 
-        totalDebt = 0.0 ;
+        totalDebt = 0.0;
 
-        if (debts!=null){
-            for (Double debt : debts){
+        if (debts != null) {
+            for (Double debt : debts) {
 
                 totalDebt += debt;
             }
-            currentBalance = totalPayment-totalDebt;
+            currentBalance = totalPayment - totalDebt;
 
         }
         return totalDebt;
     }
-    public void pay(Double money){
 
-        addPaymentsList(money);
-        currentBalance += money;
+    public void pay(Double money) {
+        if (payments.isEmpty()) {
+
+
+            if(money<500){
+                System.out.println("Your first payment cant be lower than 500");
+            }
+            else{
+                addPaymentsList(money);
+                currentBalance += money;
+            }
+
+        }
+        else {
+            addPaymentsList(money);
+            currentBalance += money;
+        }
+
     }
-    public void borrowMoney(Double money){
+
+    public void borrowMoney(Double money) {
         addDebtsList(money);
-        currentBalance -=money;
+        currentBalance -= money;
     }
-    public void addTakenBook(Book book){
+
+    public void addTakenBook(Book book) {
         takenBooks.add(book);
     }
-    public void addGivenBook(Book book){
+
+    public void addGivenBook(Book book) {
         givenBooks.add(book);
     }
-    public void addDebtsList(Double money){debts.add(money);}
-    public void addPaymentsList(Double money){payments.add(money);}
+
+    public void addDebtsList(Double money) {
+        debts.add(money);
+    }
+
+    public void addPaymentsList(Double money) {
+        payments.add(money);
+    }
 
     @Override
     public String toString() {
-        return "FinancialCheck{" +"\n"+
-                "currentBalance=" + getCurrentBalance() +"\n"+
-                "payments=" + getPayments() + "\n"+
-                "totalPayment=" + getTotalPayment() +"\n"+
-                "debts=" + getDebts() +"\n"+
-                "totalDebt=" + getTotalDebt() +"\n"+
-                "takenBooks=" + takenBooks +"\n"+
-                "givenBooks=" + givenBooks +"\n"+
+        return "FinancialCheck{" + "\n" +
+                "currentBalance=" + getCurrentBalance() + "\n" +
+                "payments=" + getPayments() + "\n" +
+                "totalPayment=" + getTotalPayment() + "\n" +
+                "debts=" + getDebts() + "\n" +
+                "totalDebt=" + getTotalDebt() + "\n" +
+                "takenBooks=" + takenBooks + "\n" +
+                "givenBooks=" + givenBooks + "\n" +
 
                 '}';
     }
